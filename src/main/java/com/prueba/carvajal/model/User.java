3f8634b9f29da1e4c03117fbce8ef49b93108781
Arturo Muñoz <arturo790_@hotmail.com>
2023-07-31 10:25:04 -0500

    Primer commit

---
+++ src/main/java/com/prueba/carvajal/model/User.java
@@ -0,0 +1,54 @@
+package com.prueba.carvajal.model;
+
+import java.util.Collection;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "users")
+public class User implements UserDetails {
+	
+	private static final long serialVersionUID = 326986364231542758L;
+
+	private String username;
+	
+	private String email;
+	
+	private String password;
+	
+	private Boolean enabled;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
